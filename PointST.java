0c5c386768cf6f8a46b330b88705c6b2c68b6c06
robertsabum <rjsabum@noctrl.edu>
2021-03-20 17:43:28 -0500

    Changes to PointST

--- PointST.java
+++ PointST.java
@@ -1,14 +1,13 @@
 import java.util.Comparator;
+import java.util.Vector;
 
 public class PointST<Value> {
 
-		private RedBlackBST<Point2D> points;
-
-
+		private RedBlackBST<Point2D, Value> points;
 
     // construct an empty symbol table of points 
     public PointST(){
-			points = new RedBlackBST<Point2D>();
+			points = new RedBlackBST<Point2D, Value>();
 		}
 
     // is the symbol table empty? 
@@ -33,27 +32,32 @@ public class PointST<Value> {
 
     // does the symbol table contain point p? 
     public boolean contains(Point2D p){
-			points.contains(p);
+			return points.contains(p);
 		}
 
     // all points in the symbol table 
     public Iterable<Point2D> points(){
+      Vector<Point2D> allpoints = new Vector<>();
 
+      return allpoints;
 		}
 
     // all points that are inside the rectangle (or on the boundary) 
     public Iterable<Point2D> range(RectHV rect){
+      Vector<Point2D> pointsinrange = new Vector<>();
+
+      return pointsinrange;
 
 		}
 
     // a nearest neighbor of point p; null if the symbol table is empty 
     public Point2D nearest(Point2D p){
-
+      return p;
 		}
 
     // unit testing (required)
     public static void main(String[] args){
-
+      
 		}
 
 }
